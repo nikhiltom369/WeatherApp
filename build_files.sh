45043eddd323125a093d5e1145205efa7e607fae
NIKHIL TOM <118368860+nikhiltom369@users.noreply.github.com>
2025-06-09 22:21:57 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # build_files.sh
-pip install -r requirements.txt
-python WeatherProject/manage.py collectstatic --noinput
+/opt/vercel/python3/bin/pip install -r requirements.txt
+/opt/vercel/python3/bin/python WeatherProject/manage.py collectstatic --noinput
