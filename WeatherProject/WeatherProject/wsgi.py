e2773f1d8048dc1df6442c32eff0f5e1c79f26c3
NIKHIL TOM <118368860+nikhiltom369@users.noreply.github.com>
2025-06-09 22:33:03 +0530

    Update wsgi.py

--- WeatherProject/WeatherProject/wsgi.py
+++ WeatherProject/WeatherProject/wsgi.py
@@ -1,19 +1,12 @@
-"""
-WSGI config for WeatherProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the project directory to the sys.path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeatherProject.settings')
 
 application = get_wsgi_application()
-
-# Add this for Vercel
-app = application
+app = application  # For Vercel
